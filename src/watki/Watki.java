0f4e3202999757d69c612b2615043a778fe4f98a
Jacek Kawa <jkawa@polsl.pl>
2012-03-27 22:25:00 +0200

    Konczenie zadan

--- src/watki/Watki.java
+++ src/watki/Watki.java
@@ -1,5 +1,8 @@
 package watki;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
 import java.util.concurrent.*;
 
 
@@ -15,52 +18,104 @@ public class Watki {
     public static void main(String[] args) throws Exception {
 
         ExecutorService zarzadzca;
-        //zarzadzca = Executors.newSingleThreadExecutor();
-        //zarzadzca.execute(new inneZadanie());
         
         zarzadzca = Executors.newCachedThreadPool();
-        zarzadzca.execute(new inneZadanie());
-        zarzadzca.execute(new zadanie());
         
+        // zadanie ze sleep
+        zarzadzca.execute(new zadanieZawieszoneSleep());
         
-        System.out.println("Koniec Main!");
+        // zadania obslugujace wejscie wyjscie roznego rodzaju
+        zarzadzca.execute(new zadanieBlokowaneIO(System.in));
+        
+        Socket gniazdoPolaczenia = new Socket("157.158.17.12", 10103);
+        InputStream netIS = gniazdoPolaczenia.getInputStream();
+        zarzadzca.execute(new zadanieBlokowaneIO(netIS));
+        
+        // zadanie, ktore sprawdza, czy powinno sie skonczyc
+        zadanieSwiadome zs = new zadanieSwiadome();
+        zarzadzca.execute(zs);
+        
+        // najpierw przerwij zadanie "swiadome"
+        TimeUnit.MILLISECONDS.sleep(1);
+        zs.czyPrzerwac = true;
         
         // przerwanie watkow zawieszonych
         TimeUnit.MILLISECONDS.sleep(1000);
         zarzadzca.shutdownNow();
+
+        // proba zamkniecia watkow zawieszonych na operacjach IO na System.IN
+        TimeUnit.MILLISECONDS.sleep(2000);
+        System.out.println("zamykam strumien in");
+        System.in.close();
+        
+        // proba zamkniecia watkow zawieszonych na operacjach IO na gniezdzie 
+        TimeUnit.MILLISECONDS.sleep(1000);
+        gniazdoPolaczenia.close();
+        
+        System.out.println("Koniec Main!");        
         
     }
     
     
-    
-    // klasa wewnetrzna z zadaniem 1
-    private static class zadanie extends Thread
+    // klasa wewnetrzna z zadaniem, ktore sprawdza, czy powinno zostac wylaczone
+    private static class zadanieSwiadome implements Runnable
     {
-
+        public volatile boolean czyPrzerwac = false;
+        
         @Override
         public void run() {
-            try {
-                sleep(5000);
-            } catch (InterruptedException ex) {
-                System.out.println("nie minelo 5s...");
+            System.out.println("rozpoczynam zadanie SWIADOME");
+            while (czyPrzerwac == false)
+            {
+                System.out.println("*");
             }
-            System.out.println("Dzialam!");
+            
+            System.out.println("zakonczylem dzialanie swiadomie");
+            
         }
         
     }
     
-    private static class inneZadanie implements Runnable
+    
+    // klasa wewnetrzna z zadaniem blokowanym na IO
+    private static class zadanieBlokowaneIO implements Runnable
+    {
+        private InputStream ins;
+        public zadanieBlokowaneIO(InputStream ins)
+        {
+            this.ins = ins;
+        }
+        
+        @Override
+        public void run() {
+            System.out.println("Zadanie z blokowanym IO na " + ins.getClass().getName());
+            try {
+                while (true)
+                    ins.read();
+            } catch (IOException ex) {
+                if (Thread.currentThread().isInterrupted())
+                    System.out.println("bylo zgloszone przerwanie ale i tak czekalismy!");
+                System.out.println("wyjatek IO zwiazany z !" + ins.getClass().getName());
+            } 
+            
+        }
+        
+    }
+    // klasa wewnetrzna z zadaniem przysypiajacym
+    private static class zadanieZawieszoneSleep implements Runnable
     {
 
         @Override
         public void run() {
+            System.out.println("Zadanie z przysypianiem");
             try {
-                Thread.sleep(5000);
+                TimeUnit.MILLISECONDS.sleep(5000);
+                System.out.println("Zadanie z przypisaniem wypisalo komunikat");
             } catch (InterruptedException ex) {
-                System.out.println("Nie minelo 5s (2)...");
+                System.out.println("Nie minelo 5s, ale ktos nam przerwal...");
             }
             
-            System.out.println("Ja też działam!");
+            
         }
         
     }
